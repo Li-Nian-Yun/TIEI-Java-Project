a34ea3f1febf734c51431410594ea8077a85daf7
li-nianyun <948354258@qq.com>
2023-12-10 18:24:09 +0800

    需要用到的文件

---
+++ project/project/CompressFile.java
@@ -0,0 +1,24 @@
+package project;
+import java.util.PriorityQueue;
+/**
+ * A class for file compression
+ */
+public class CompressFile {
+
+    /**
+     * Makes a file compressor
+     */
+    public CompressFile() {
+       
+    }
+
+    /**
+     * Compress a file
+     */
+    public void compress() {
+    	System.out.println("file compression");
+    }
+
+
+
+}
---
+++ project/project/UncompressFile.java
@@ -0,0 +1,21 @@
+package project;
+
+import java.io.*;
+
+/**
+ * A class for file uncompression
+ */
+public class UncompressFile {
+
+
+    public UncompressFile() {
+        
+    }
+
+    /**
+     * Uncompress a file
+     */
+    public void uncompress() {
+    	System.out.println("file uncompression");
+    }
+}
