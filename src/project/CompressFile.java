a21af8dd2f7eca9ebd98373c578697f21e217453
li-nianyun <948354258@qq.com>
2023-12-16 16:10:34 +0800

    change

--- src/project/CompressFile.java
+++ src/project/CompressFile.java
@@ -23,7 +23,7 @@ public class CompressFile {
      * Compress a file
      */
     public void compress(File inputFile) {
-        File outputFile =  new File(inputFile.toString()+".hu");
+        File outputFile =  new File(inputFile.toString()+".hua");
         zipFile(inputFile,outputFile);
         System.out.println("file compression");
     }
@@ -74,9 +74,10 @@ public class CompressFile {
             data /= BYTE_MAX;
             length++;
         }
-        byte[] magicNumber = new byte[length];
-        for(int i = 0; i < length; i++){
-            magicNumber[i] = (byte) (MAGIC_NUMBER >> (length - i - 1) * 8);
+        byte[] magicNumber = new byte[length+1];
+        magicNumber[0] = (byte) length;
+        for(int i = 1; i < length+1; i++){
+            magicNumber[i] = (byte) (MAGIC_NUMBER >> (length - i) * 8);
         }
         return magicNumber;
     }
--- src/project/test.java
+++ src/project/test.java
@@ -5,10 +5,26 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class test {
     public static void main(String[] args) throws FileNotFoundException {
-
+        File filePath = new File("./files/image.png.hu");
+        Map<Byte,Integer> counts=new HashMap<>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            byte[] buffer = new byte[3096];
+            while ( fileInputStream.read(buffer) != -1) {
+                for(byte item:buffer){
+                    System.out.println(item);
+                }
+//                fileInputStream.close();
+            }
+            fileInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
